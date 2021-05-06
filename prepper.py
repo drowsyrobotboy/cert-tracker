a5ab5469ef9c628e77ad42a0f1d0b3587ac9459f
maruthisharma <maruthisharma@live.in>
2021-05-07 00:58:46 +0530

    new approach using get ssl cert

--- prepper.py
+++ prepper.py
@@ -4,38 +4,35 @@ import os
 import subprocess
 import sys
 import OpenSSL
+import ssl, socket
 import json
 from flask import Flask, request, jsonify
 
 # Define the Cert class
 class Cert:
-    def __init__(self, name, expiry):
-        self.name = name
+    def __init__(self, hostname, port, expiry):
+        self.hostname = hostname
+        self.port = port
         self.expiry = expiry
 
-cmd = "grep -i SSLCertificateFile "+sys.argv[1]+" | grep -v ^\# | xargs"
+cmd = "grep -i ServerName "+sys.argv[1]+" | grep -v ^\# | xargs"
 ps = subprocess.Popen(cmd,shell=True,stdout=subprocess.PIPE,stderr=subprocess.STDOUT)
 output = ps.communicate()[0]
-# The following command prints as b'...'. We need to decode the bytes
-# print(output)
 
-arr = (output.decode('utf-8').split("SSLCertificateFile")) # get an array of all certs
+arr = (output.decode('utf-8').split("ServerName")) # get an array of all certs
 arr.pop(0) # remove first entry as it will be empty
 dist_arr = list(set(arr)) # converting to a set to remove dups
 arr = [] # empty the original array to fill with Cert objects
 
 for x in dist_arr:
     x = x.strip()
-    with open(x, "r") as cert_file:
-        cert_text = cert_file.read()
-        x509 = OpenSSL.crypto.load_certificate(OpenSSL.crypto.FILETYPE_PEM, cert_text)
+    cert_text=ssl.get_server_certificate((x.split(":")[0], x.split(":")[1]),ssl_version=ssl.PROTOCOL_TLSv1_2)
+    x509 = OpenSSL.crypto.load_certificate(OpenSSL.crypto.FILETYPE_PEM, cert_text)
+
     # append the json version of Cert object to the array. Using .__dict_ to convert the object to json_
-    ct = Cert(x,x509.get_notAfter().decode('utf-8'))
+    ct = Cert(x.split(":")[0], x.split(":")[1],x509.get_notAfter().decode('utf-8'))
     arr.append(json.dumps(ct.__dict__))
-# Printing on console
-#
-#for c in arr:
-#    print(c.name, c.expiry)
+
 
 # Web using Flaskapp
 app = Flask(__name__)
